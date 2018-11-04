be563e2a6e6e8e7b50bc5e71f19949776ae7821c
Jean Wanka <jm.wanka@gmail.com>
2018-11-04 22:40:09 +0100

    fix typo

--- unet_parts_torch.py
+++ unet_parts_torch.py
@@ -23,7 +23,7 @@ class double_conv(nn.Module):
 class single_conv(nn.Module):
     '''(conv => BN => ReLU) * 2'''
     def __init__(self, in_ch, out_ch):
-        super(double_conv, self).__init__()
+        super(single_conv, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_ch, out_ch, 3, padding=1),
             nn.BatchNorm2d(out_ch),
